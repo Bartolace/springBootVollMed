96ee7deb88d50d4c885f0447b121451976ef3d99
gabrielBartolace <gabrielll.freitas@hotmail.com>
2023-02-25 15:38:30 -0300

    Entitys and repositorys, yml
    medico e endereco

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,20 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.entity.Medico;
 import med.voll.api.medico.records.DadosCadastroMedico;
+import med.voll.api.medico.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
