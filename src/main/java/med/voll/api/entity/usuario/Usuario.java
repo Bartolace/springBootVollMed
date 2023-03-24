89ce08e0760be0f6111733ff573a89e3adb339ae
990845 <gabrielbartolace@castgroup.com.br>
2023-03-24 10:00:14 -0400

    table usuarios

---
+++ src/main/java/med/voll/api/entity/usuario/Usuario.java
@@ -0,0 +1,18 @@
+package med.voll.api.entity.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String loguin;
+    private String senha;
+}
